98b40d3ee9576f67ac45b9228680a1e5e0c63679
Ghazaleh Ranjbaran <ranjbaran.gh@gmail.com>
2018-08-30 18:31:37 +0430

    change main.py

--- homePage-getData/main.py
+++ homePage-getData/main.py
@@ -5,11 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello, World!'
+    url = "http://digikala.com"
+    result = requests.get(url).content
+    return result
+	#return 'Hello, World!'
 
-"""
-@app.route('/check/<url>')
-def check(url):
-    return render_template('hello.html', name=name)
-"""
+@app.route('/', methods=['POST', 'GET'])
+def my_form_post():
+    if request.method == 'POST' :
+        url = request.form['url']
+        url = "digikala.com"
+        user_count = request.form['user_count']
+        result = request.get(url)
+        return result
 
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=8009)
