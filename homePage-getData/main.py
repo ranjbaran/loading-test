d7167cbbde4478f2ced56ad465f79f59f6eb412e
Ghazaleh Ranjbaran <ranjbaran.gh@gmail.com>
2018-08-30 20:04:52 +0430

    install requests library

--- homePage-getData/main.py
+++ homePage-getData/main.py
@@ -1,22 +1,24 @@
 from flask import Flask
+import requests
+from datetime import datetime
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    url = "http://digikala.com"
-    result = requests.get(url).content
-    return result
+    url = "http://www.digikala.com"
+    result = requests.get(url)
+    return result.elapsed.total_seconds()
 	#return 'Hello, World!'
+
 
 @app.route('/', methods=['POST', 'GET'])
 def my_form_post():
     if request.method == 'POST' :
         url = request.form['url']
-        url = "digikala.com"
         user_count = request.form['user_count']
-        result = request.get(url)
+        result = requests.get('http://' + url)
         return result
 
 if __name__ == '__main__':
